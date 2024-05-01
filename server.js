59d506a34a876588e9ff05f240404ad5f710fba1
dinkien <21522244@gm.uit.edu.vn>
2024-05-01 21:43:13 +0700

    change server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const couponRoute = require("./routes/couponRoute");
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 app.use(
