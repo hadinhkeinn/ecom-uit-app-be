3fd07e1d06461332e3b6eacdfcc315b564ae91cf
Gia Bao <21521866@gm.uit.edu.vn>
2024-05-08 09:54:51 +0700

    chore: Update productModel.js schema for improved readability and maintainability

--- models/productModel.js
+++ models/productModel.js
@@ -1,64 +1,54 @@
 const e = require("express");
 const mongoose = require("mongoose");
 
-
-const productSchema = mongoose.Schema({
+const productSchema = mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: [true, "Please add a name"],
-        trim: true,
+      type: String,
+      required: [true, "Please add a name"],
+      trim: true,
     },
     category: {
-        type: String,
-        required: [true, "Please add a category"],
-        trim: true,
-    },
-    brand: {
-        type: String,
-        required: [true, "Please add a brand"],
-        trim: true,
-    },
-    color: {
-        type: String,
-        required: [true, "Please add a color"],
-        default: "As seen",
-        trim: true,
+      type: String,
+      required: [true, "Please add a category"],
+      trim: true,
     },
     quantity: {
-        type: Number,
-        required: [true, "Please add a quantity"],
-        trim: true,
+      type: Number,
+      required: [true, "Please add a quantity"],
+      trim: true,
     },
     sold: {
-        type: Number,
-        default: 0,
-        trim: true,
+      type: Number,
+      default: 0,
+      trim: true,
     },
     regularPrice: {
-        type: Number,
-        // required: [true, "Please add a regular price"],
-        trim: true,
+      type: Number,
+      // required: [true, "Please add a regular price"],
+      trim: true,
     },
     price: {
-        type: Number,
-        required: [true, "Please add a price"],
-        trim: true,
+      type: Number,
+      required: [true, "Please add a price"],
+      trim: true,
     },
     description: {
-        type: String,
-        required: [true, "Please add a description"],
-        trim: true,
+      type: String,
+      required: [true, "Please add a description"],
+      trim: true,
     },
     image: {
-        type: [String],
+      type: [String],
     },
     ratings: {
-        type: [Object],
+      type: [Object],
     },
-},
-    {
-        timestamps: true,
-    })
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const Product = mongoose.model("Product", productSchema);
 module.exports = Product;
